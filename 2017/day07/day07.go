197cc83e7b4e1c3f6c222cc4a6f2b49ff21572c1
Tyler Plude <tsplude@gmail.com>
2017-12-15 23:08:47 -0500

    messy day07 part2 initial work

--- 2017/day07/day07.go
+++ 2017/day07/day07.go
@@ -5,11 +5,12 @@ import (
 	"fmt"
 	"github.com/deckarep/golang-set"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func part_one() string {
-	file, _ := os.Open("data.txt")
+func part_one(input_file string) string {
+	file, _ := os.Open(input_file)
 	scanner := bufio.NewScanner(file)
 
 	lhs := mapset.NewSet()
@@ -46,11 +47,61 @@ func part_one() string {
 	return lhs.Difference(rhs).String()
 }
 
-func part_two() bool {
-	return false
+func find_weight(w_map map[string]int, n_map map[string][]string, id string) int {
+	if _, ok := n_map[id]; !ok {
+		return w_map[id]
+	}
+
+	w_sum := w_map[id]
+	for _, n := range n_map[id] {
+		w_sum += find_weight(w_map, n_map, n)
+	}
+	return w_sum
+}
+
+func part_two(input_file string) string {
+	file, _ := os.Open(input_file)
+	scanner := bufio.NewScanner(file)
+
+	weight_map := make(map[string]int)
+	neighbor_map := make(map[string][]string)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// get LHS id
+		lhs_id := strings.Split(line, " ")[0]
+
+		// get LHS weight
+		lidx := strings.Index(line, "(") + 1
+		ridx := strings.Index(line, ")")
+		w, _ := strconv.Atoi(line[lidx:ridx])
+
+		_, ok := weight_map[lhs_id]
+		if !ok {
+			weight_map[lhs_id] = w
+		}
+
+		if strings.Count(line, "->") > 0 {
+			neighbors := []string{}
+			neighbors_str := strings.Split(line, "->")[1]
+			for _, n := range strings.Split(neighbors_str, ",") {
+				neighbors = append(neighbors, strings.Trim(n, ", "))
+			}
+
+			_, ok := neighbor_map[lhs_id]
+			if !ok {
+				neighbor_map[lhs_id] = neighbors
+			}
+		}
+	}
+
+	fmt.Println("mkxke:", find_weight(weight_map, neighbor_map, "mkxke"))
+
+	return "not yet"
 }
 
 func main() {
-	fmt.Println("Part 1:", part_one())
-	fmt.Println("Part 2:", part_two())
+	fmt.Println("Part 1:", part_one("input.txt"))
+	fmt.Println("Part 2:", part_two("input.txt"))
 }
