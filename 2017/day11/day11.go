5307d27f71f334214ca99498903a51d8244007c0
Tyler Plude <tsplude@gmail.com>
2017-12-23 01:49:40 -0500

    better day11 part1 attempt

--- 2017/day11/day11.go
+++ 2017/day11/day11.go
@@ -4,10 +4,16 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 )
 
 var DATA_FILE string = os.Args[1]
+
+func regexp_replace(target, re_str, re_replace string) string {
+	re := regexp.MustCompile(re_str)
+	return re.ReplaceAllLiteralString(target, re_replace)
+}
 
 func part_one(data_file string) bool {
 	file, _ := os.Open(data_file)
@@ -15,36 +21,47 @@ func part_one(data_file string) bool {
 	reader := bufio.NewReader(file)
 
 	input, _ := reader.ReadString('\n')
-	steps := strings.Split(strings.TrimSpace(input), ",")
-	fmt.Println("Steps:", steps)
+	steps_str := strings.TrimSpace(input)
+	fmt.Println("Steps:", steps_str)
 
-	x, y, z := 0, 0, 0
-	for _, step := range steps {
-		fmt.Println(step)
-		switch step {
-		case "n":
-			fmt.Println("case 'n'")
-			y += 1
-		case "ne":
-			fmt.Println("case 'ne'")
-			x += 1
-		case "se":
-			fmt.Println("case 'se'")
-			z += 1
-		case "s":
-			fmt.Println("case 's'")
-			y -= 1
-		case "sw":
-			fmt.Println("case 'sw'")
-			x -= 1
-		case "nw":
-			fmt.Println("case 'nw'")
-			z -= 1
-		default:
-			panic(fmt.Sprintf("Unsupported step %s", step))
+	replacement_map := map[string]string{
+		"n,s,|n,s$":     "",
+		"n,se":          "ne",
+		"n,sw":          "nw",
+		"nw,se,|nw,se$": "",
+		"nw,ne":         "n",
+		"nw,s":          "sw",
+		"sw,ne,|sw,ne$": "",
+		"sw,n":          "nw",
+		"sw,se":         "s",
+		"s,n,|s,n$":     "",
+		"s,nw":          "sw",
+		"s,ne":          "se",
+		"se,nw,|se,nw$": "",
+		"se,sw":         "s",
+		"se,n":          "ne",
+		"ne,sw,|ne,sw$": "",
+		"ne,nw":         "n",
+		"ne,s":          "se",
+	}
+
+	before_len := len(steps_str)
+	for {
+		for re_str, repl := range replacement_map {
+			after_str := regexp_replace(steps_str, re_str, repl)
+
+			fmt.Printf("RE: %s  W: %s  BEF: %s  AFT: %s\n", re_str, repl, steps_str, after_str)
+			steps_str = after_str
+		}
+		if before_len == len(steps_str) {
+			break
+		} else {
+			before_len = len(steps_str)
 		}
 	}
-	fmt.Printf("x: %d y: %d z: %d\n", x, y, z)
+
+	fmt.Println("After:", steps_str)
+
 	return false
 }
 
